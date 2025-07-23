3f8fd11b1dbc5ac9a4273220a20ba3d9c5798001
Hoang Viet Duong <duongviethoang.2611@gmail.com>
2025-07-23 14:52:31 +0700

    feat: using markup to display html

--- web_framework/awesome_owl/static/src/playground.js
+++ web_framework/awesome_owl/static/src/playground.js
@@ -1,11 +1,16 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
+
+  setup() {
+    this.string1 = "<div class='text-primary'>some content</div>";
+    this.string2 = markup("<div class='text-primary'>some content</div>");
+  }
 
   static components = { Counter, Card };
 }
