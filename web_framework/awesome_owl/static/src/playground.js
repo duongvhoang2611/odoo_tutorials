8fc7546221545ca83cc9f3d09895c68ed99a5f10
Hoang Viet Duong <duongviethoang.2611@gmail.com>
2025-07-23 15:35:35 +0700

    feat: sum of two counter

--- web_framework/awesome_owl/static/src/playground.js
+++ web_framework/awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, markup } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -10,7 +10,12 @@ export class Playground extends Component {
   setup() {
     this.string1 = "<div class='text-primary'>some content</div>";
     this.string2 = markup("<div class='text-primary'>some content</div>");
+    this.sum = useState({ value: 2 });
   }
 
   static components = { Counter, Card };
+
+  incrementSum() {
+    this.sum.value++;
+  }
 }
