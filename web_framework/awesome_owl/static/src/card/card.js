35d03396e651b942c16a9970cd0d2f5e00d80a7c
Hoang Viet Duong <duongviethoang.2611@gmail.com>
2025-07-24 11:30:30 +0700

    feat(card): add a toggle button

--- web_framework/awesome_owl/static/src/card/card.js
+++ web_framework/awesome_owl/static/src/card/card.js
@@ -3,10 +3,17 @@ import { Component, useState } from "@odoo/owl";
 export class Card extends Component {
   static template = "awesome_owl.card";
 
+  setup() {
+    this.state = useState({ isOpen: true });
+  }
   static props = {
     title: String,
     slots: {
       type: Object,
     },
   };
+
+  toggleContent() {
+    this.state.isOpen = !this.state.isOpen;
+  }
 }
