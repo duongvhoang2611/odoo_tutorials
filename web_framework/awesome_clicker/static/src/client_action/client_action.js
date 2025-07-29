50ad0fe249869f47fc034de6545816582107dbe2
Hoang Viet Duong <duongviethoang.2611@gmail.com>
2025-07-29 10:38:28 +0700

    format the displayed value

--- web_framework/awesome_clicker/static/src/client_action/client_action.js
+++ web_framework/awesome_clicker/static/src/client_action/client_action.js
@@ -1,10 +1,13 @@
 import { Component } from '@odoo/owl'
 import { registry } from '@web/core/registry'
 import { useClicker } from '../clicker_hook'
+import { ClickerValue } from '../clicker_value/clicker_value'
 
 export class ClickerClientAction extends Component {
   static template = 'awesome_clicker.client_action'
   static props = ['*']
+
+  static components = { ClickerValue }
 
   setup() {
     this.clicker = useClicker()
