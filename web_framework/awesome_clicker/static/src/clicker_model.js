23b4636ce6607343ba4f1fdfed528998dc7df04d
Hoang Viet Duong <duongviethoang.2611@gmail.com>
2025-07-29 15:00:54 +0700

    feat: Add milestone event bus and effect notification for 1k clicks

--- web_framework/awesome_clicker/static/src/clicker_model.js
+++ web_framework/awesome_clicker/static/src/clicker_model.js
@@ -1,4 +1,5 @@
 import { Reactive } from '@web/core/utils/reactive'
+import { EventBus } from '@odoo/owl'
 
 export class ClickerModel extends Reactive {
   constructor() {
@@ -13,11 +14,16 @@ export class ClickerModel extends Reactive {
     setInterval(() => {
       this.counter += this.clickBots * 10
     }, 10 * 1000)
+
+    this.bus = new EventBus()
   }
 
   increment(inc) {
     this.counter += inc
-    if (this.level < 1 && this.counter >= 1000) this.level++
+    if (this.level < 1 && this.counter >= 1000) {
+      this.bus.trigger('MILESTONE_1k')
+      this.level++
+    }
   }
 
   buyClickBot() {
